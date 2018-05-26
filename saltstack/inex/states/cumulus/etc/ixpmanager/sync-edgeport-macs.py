67b37d3968abb2113d764fcf698ac8ad47ff650e
Nick Hilliard <nick@foobar.org>
2018-05-26 15:31:50 +0100

    update license text

--- saltstack/inex/states/cumulus/etc/ixpmanager/sync-edgeport-macs.py
+++ saltstack/inex/states/cumulus/etc/ixpmanager/sync-edgeport-macs.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
-#
+##
+## Copyright (C) 2018 Internet Neutral Exchange Association Company Limited By Guarantee.
+## All Rights Reserved.
+##
+## This file is part of IXP Manager.
+##
+## IXP Manager is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by the Free
+## Software Foundation, version 2.0 of the License.
+##
+## IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+## more details.
+##
+## You should have received a copy of the GNU General Public License v2.0
+## along with IXP Manager.  If not, see:
+##
+## http://www.gnu.org/licenses/gpl-2.0.html
+##
+## --
+##
 # FIXME: a mac address can appear on another interface.  E.g.  local mac
 # address wired to swp1 could also be pinned to another interface where the
 # mac address was auto-learned.  Fixing this may require knowledge of the
